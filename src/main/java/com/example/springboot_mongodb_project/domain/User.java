cad3c80c291488eeeaeeca6082bc7d8ba3ec113b
Kevin Santos <kevinkevinbarbosa@gmail.com>
2024-08-20 16:48:01 -0300

    Get_All_Funcionando

--- src/main/java/com/example/springboot_mongodb_project/domain/User.java
+++ src/main/java/com/example/springboot_mongodb_project/domain/User.java
@@ -3,8 +3,15 @@ package com.example.springboot_mongodb_project.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="user")
 //Class User
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
     private String id;
     private String name;
     private String email;
--- src/main/java/com/example/springboot_mongodb_project/resourcers/UserResourcers.java
+++ src/main/java/com/example/springboot_mongodb_project/resourcers/UserResourcers.java
@@ -1,6 +1,8 @@
 package com.example.springboot_mongodb_project.resourcers;
 
 import com.example.springboot_mongodb_project.domain.User;
+import com.example.springboot_mongodb_project.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,13 +15,13 @@ import java.util.List;
 @RestController
 @RequestMapping(value="/users")
 public class UserResourcers {
+
+    @Autowired
+    private UserService service;
     @RequestMapping(method= RequestMethod.GET)
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1", "maria", "maria@mail.com");
-        User jose = new User("2", "jose", "jose@mail.com");
 
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, jose));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
