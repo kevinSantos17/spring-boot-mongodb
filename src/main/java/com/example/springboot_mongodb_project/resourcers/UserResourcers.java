e9143c20bf0d0a0e95cc4a32b0d018ba3d2013fa
Kevin Santos <kevinkevinbarbosa@gmail.com>
2024-08-20 19:36:40 -0300

    DTO adicionado ao projeto

--- src/main/java/com/example/springboot_mongodb_project/resourcers/UserResourcers.java
+++ src/main/java/com/example/springboot_mongodb_project/resourcers/UserResourcers.java
@@ -1,6 +1,7 @@
 package com.example.springboot_mongodb_project.resourcers;
 
 import com.example.springboot_mongodb_project.domain.User;
+import com.example.springboot_mongodb_project.dto.UserDto;
 import com.example.springboot_mongodb_project.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -19,9 +21,10 @@ public class UserResourcers {
     @Autowired
     private UserService service;
     @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDto>> findAll(){
 
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
